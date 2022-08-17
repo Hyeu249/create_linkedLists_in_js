d9cbef3a3d48ab85f064bfdb6107dc337219560b
BaoHieu <labaohieu@gmail.com>
2022-08-17 16:35:56 +0700

    first code

---
+++ index.js
@@ -0,0 +1,88 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode; // this.head.next === newNode
+    this.tail = newNode; // this.head.next === this.tail
+    //if keep going => tail === the last nested NEXT value of the object but NOT null(of HEAD)
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+    return array;
+  }
+  insert(index, value) {
+    //check params
+    if (index >= this.length) {
+      this.append(value);
+      return this.printList();
+    }
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    //check params
+    //this function not work if params === 0 && >= this.length
+    //linkedList.tail has not changed yet
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+}
+
+const myLinkedList = new LinkedList(2);
+
+myLinkedList.append(3);
+myLinkedList.prepend(1);
+
+myLinkedList.insert(0, 99);
+myLinkedList.remove(3);
+console.log(myLinkedList);
